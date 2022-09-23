1e1b5997cd0e476400fc7ddbc9d78bd49303a58c
IvanKovalev <deformgame@gmail.com>
2022-09-23 12:32:00 +0300

    Added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// SЗадача 68: Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+
+Console.Clear();
+Console.WriteLine("Введите число M : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+GreatestCommonDivisor(m, n);
+
+void GreatestCommonDivisor(int m, int n)
+{
+    if( n % m == 0 ) 
+    Console.Write($"Наибольший общий делитель равен - {m}");
+
+    else 
+    GreatestCommonDivisor(n - ( n / m ) * m, m);
+}
